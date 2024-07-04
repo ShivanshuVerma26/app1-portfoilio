e32c9169a3338ff722272b75c32204b785a77a06
Shivanshu Verma <er.shivanshu26@gmail.com>
2024-07-04 19:32:05 +0530

    Added script to send email

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -3,3 +3,10 @@ import streamlit as st
 
 st.header("Contact US")
 
+with st.form(key="my-email-form"):
+    email_address = st.text_input("Your email address")
+    email_message = st.text_area("Enter your message")
+    button = st.form_submit_button("Submit")
+
+    if button:
+        print("I am pressed!")
